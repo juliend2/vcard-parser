5b94b8ac0c307e3ef7afe3d11a5e0fb2a57ba128
Julien Desrosiers <juliend2@gmail.com>
2011-11-05 11:57:17 -0400

    parse emails, phones and notes

--- parse.rb
+++ parse.rb
@@ -1,11 +1,14 @@
 
 class Contact
-  attr_accessor :vcard_string, :full_name, :nom, :prenom, :organisation, :email, :tel, :fax, :adresse, :note, :jabber, :aim
+  attr_accessor :vcard_string, :full_name, :nom, :prenom, :organisation, :emails, :phones, :adresse, :note
 
   def initialize(vcard)
     @vcard_string = vcard
+    parse(vcard)
+  end
 
-    # NOM ET PRENOM:
+  def parse(vcard)
+    # LAST NAME AND FIRST NAME
     m_name = vcard.match(/^N:(.*);;;/)
     if m_name && m_name[1] != ';'
       (@nom,@prenom) = m_name[1].split(';')
@@ -15,25 +18,19 @@ class Contact
     # ORGANISATION:
     m_organisation = vcard.match(/^ORG:(.*);/)
     @organisation = m_organisation[1] if m_organisation
-    # EMAIL
-    m_email = vcard.match(/^(item\d.EMAIL|EMAIL).*type=pref:(.*)/)
-    @email = m_email[2] if m_email
-    # JABBER
-    m_jabber = vcard.match(/^X-JABBER.*type=pref:(.*)/)
-    @jabber = m_jabber[1] if m_jabber
-    # AIM
-    m_aim = vcard.match(/^X-AIM.*type=pref:(.*)/)
-    @aim = m_aim[1] if m_aim
-    puts m_email.length if m_email
-    #
-    # TODO: faire en sorte que les contacts qui ont plus d'une adresse email
-    # soient pris en compte, comme patricia et yegor par exemple
-    #
+    # EMAILS
+    e_reg = Regexp.new(/\b[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}\b/)   
+    @emails = vcard.scan(e_reg).uniq
+    # PHONES (will also grab faxes, but that's OK for now)
+    p_reg = Regexp.new(/\b(?:\(?(\d{3})\)?[ -]?)?(?:(\d{3})-?(\d{4}))\b/)
+    @phones = vcard.scan(p_reg).uniq
+    @phones.map!{|p| p.join('-')}
+    m_note = vcard.match(/^NOTE:(.*)/)
+    @note = m_note[1] if m_note
+    @note = @note.gsub(/\\n/,"\n").gsub(/\\/,'') if @note
   end
 end
 
 File.open('vCards.vcf').read.split("END:VCARD\nBEGIN:VCARD").each do |vcard|
   contact = Contact.new(vcard)
-
 end
-
