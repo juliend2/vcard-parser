af035a2480816f8e55d41747297df92e9ca1589f
Julien Desrosiers <juliend2@gmail.com>
2011-11-05 13:34:33 -0400

    ADDED: write markdown files into contacts directory

--- parse.rb
+++ parse.rb
@@ -1,6 +1,18 @@
 require './contact'
 
-File.open('vCards.vcf').read.split("END:VCARD\nBEGIN:VCARD").each do |vcard|
+INPUT_FILENAME = 'vCards.vcf'
+DIRNAME = 'contacts'
+
+begin
+  Dir::mkdir(DIRNAME)
+rescue
+  puts "the '#{DIRNAME}' directory already exists"
+end
+
+File.open(INPUT_FILENAME).read.split("END:VCARD\nBEGIN:VCARD").each do |vcard|
   contact = Contact.new(vcard)
-  puts contact.to_markdown
+  output_file_name = contact.full_name.downcase.strip.gsub(/\W/, '-').chomp('-') + ".md"
+  File.open("#{DIRNAME}/#{output_file_name}", 'w') {|f| 
+    f.write(contact.to_markdown)
+  }
 end
